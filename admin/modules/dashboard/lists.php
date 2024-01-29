c3131e0addd0eed403be4ef110489a6f59007b4f
TonThanhTam <tamtt1512@gmail.com>
2024-01-29 09:27:24 +0700

    [dashboard] : chỉnh sửa dashboard lấy thông tin thực tế

--- admin/modules/dashboard/lists.php
+++ admin/modules/dashboard/lists.php
@@ -17,6 +17,11 @@ if(!isLogin()) {
 
    $signText = firstRaw("SELECT sign_text FROM sign WHERE user_id = $userId");
 
+   $countUsers = getRows("SELECT id FROM users");
+   $countProducts = getRows("SELECT id FROM products");
+   $countFactories = getRows("SELECT id FROM factories");
+   $countReports = getRows("SELECT id FROM reports");
+
    $message = getFlashData('msg');
    $msgType = getFlashData('msg_type');
    if(empty($signText)) {
@@ -36,14 +41,15 @@ if(!isLogin()) {
             <!-- small box -->
             <div class="small-box bg-info">
                <div class="inner">
-                  <h3>150</h3>
+                  <h3><?php echo !empty($countProducts) ? $countProducts : 0 ?></h3>
 
-                  <p>New Orders</p>
+                  <p>Sản Phẩm</p>
                </div>
                <div class="icon">
                   <i class="ion ion-bag"></i>
                </div>
-               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+               <a href="<?php echo getLinkAdmin('products') ?>" class="small-box-footer">Xem Thêm <i
+                     class="fas fa-arrow-circle-right ml-2"></i></a>
             </div>
          </div>
          <!-- ./col -->
@@ -51,14 +57,15 @@ if(!isLogin()) {
             <!-- small box -->
             <div class="small-box bg-success">
                <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                  <h3><?php echo !empty($countFactories) ? $countFactories : 0?></h3>
 
-                  <p>Bounce Rate</p>
+                  <p>Cơ Sở Gia Công</p>
                </div>
                <div class="icon">
                   <i class="ion ion-stats-bars"></i>
                </div>
-               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+               <a href="<?php echo getLinkAdmin('factories') ?>" class="small-box-footer">Xem Thêm<i
+                     class="fas fa-arrow-circle-right ml-2"></i></a>
             </div>
          </div>
          <!-- ./col -->
@@ -66,14 +73,15 @@ if(!isLogin()) {
             <!-- small box -->
             <div class="small-box bg-warning">
                <div class="inner">
-                  <h3>44</h3>
+                  <h3><?php echo !empty($countUsers) ? $countUsers : 0 ?></h3>
 
-                  <p>User Registrations</p>
+                  <p>Người Dùng</p>
                </div>
                <div class="icon">
-                  <i class="ion ion-person-add"></i>
+                  <i class="ion ion-person"></i>
                </div>
-               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+               <a href="<?php echo getLinkAdmin('users') ?>" class="small-box-footer">Xem Thêm<i
+                     class="fas fa-arrow-circle-right ml-2"></i></a>
             </div>
          </div>
          <!-- ./col -->
@@ -81,14 +89,15 @@ if(!isLogin()) {
             <!-- small box -->
             <div class="small-box bg-danger">
                <div class="inner">
-                  <h3>65</h3>
+                  <h3><?php echo !empty($countReports) ? $countReports : 0 ?></h3>
 
-                  <p>Unique Visitors</p>
+                  <p>Biên Bản</p>
                </div>
                <div class="icon">
-                  <i class="ion ion-pie-graph"></i>
+                  <i class="fa fa-file"></i>
                </div>
-               <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+               <a href="<?php echo getLinkAdmin('reports') ?>" class="small-box-footer">Xem Thêm<i
+                     class="fas fa-arrow-circle-right ml-2"></i></a>
             </div>
          </div>
          <!-- ./col -->
